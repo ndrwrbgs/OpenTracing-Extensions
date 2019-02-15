69dcc6c2223b21f1dde5671de90cba63f4440ca9
ndrwrbgs <ndrwrbgs@users.noreply.github.com>
2019-02-14 19:28:24 -0600

    Add BuildSpanUsingMemberName - fixes #7

---
+++ src/Library/TracerExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenTracing.Contrib.Extensions
+{
+    using System.Runtime.CompilerServices;
+
+    using JetBrains.Annotations;
+
+    [PublicAPI]
+    public static class TracerExtensions
+    {
+        public static ISpanBuilder BuildSpanUsingMemberName(
+            this ITracer tracer,
+            [CallerMemberName] string memberName = null)
+        {
+            return tracer
+                .BuildSpan(memberName);
+        }
+    }
+}
